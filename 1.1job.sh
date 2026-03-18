6d9582c4ddc9e9dde7bec21174d32d23118e0837
Ravi Varikuti(PC) <vvravi07@gmail.com>
2026-03-18 15:43:29 -0400

    added job files

---
+++ 1.1job.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+#SBATCH --job-name 1.1parallel_sim
+#SBATCH --ntasks 150
+#SBATCH --cpus-per-task 1
+#SBATCH --mem-per-cpu 2gb
+#SBATCH --time 02:00:00
+
+
+module load anaconda3
+source activate jax-ai-stack
+export XLA_PYTHON_CLIENT_PREALLOCATE=false
+
+# Launch MPI program
+srun python 1.1parallel_sim.py
